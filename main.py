0de2d70458c1b5d89b8df3c71712c5bd83e1dcb1
Cuenta12345 <cuentadosxxi@gmail.com>
2026-03-03 20:00:19 +0100

    refactor: rename files and UI strings to English for international consistency

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 from groq import Groq
-from funciones_rag import load_and_vectorize, search_best_context
+from rag_functions import load_and_vectorize, search_best_context
 
 
 #Cargar la API KEY
@@ -12,18 +12,18 @@ client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 # Load and vectorize 
 text, embeddings = load_and_vectorize()
 # Make a question
-question = "¿Cual es el color favorito ?"
+question = "Which is my favorite color?"  # Change this to test new questions without using the interface.
 # Search the context
 context = search_best_context(question, text, embeddings)
 # Generate answer with Groq
 complete_promp = f"""
-Eres un asistente experto. Utiliza UNICAMENTE el siguiente contexto para reponder la pregunta.
-Si la respuesta no esta en el contexto simplmente di "No lo se".
+You are an expert assistant. Use ONLY the following context to answer the question.
+If the answer is not in the context, simply say "I don't know."
 
-CONTEXTO:
+CONTEXT:
 {context}
 
-PREGUNTA:
+QUESTION:
 {question}
 """
 
